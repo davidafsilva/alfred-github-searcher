f346b4a6c2fa2aac964e9d3dbc918014bb4a6863
David Silva <david@davidafsilva.pt>
2022-03-26 21:42:13 +0000

    adds workflow project structure

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/davidafsilva/alfred-github-top-repositories/action"
+	aw "github.com/deanishe/awgo"
+	awu "github.com/deanishe/awgo/update"
+)
+
+var (
+	githubRepository = "davidafsilva/alfred-github-repositories"
+	githubIssues     = fmt.Sprintf("%s/issues", githubRepository)
+	wf               *aw.Workflow
+)
+
+func init() {
+	wf = aw.New(awu.GitHub(githubRepository), aw.HelpURL(githubIssues), aw.MaxResults(10))
+}
+
+func run() {
+	name := wf.Args()[0]
+	var err error = nil
+	switch name {
+	case "search":
+		err = action.Search(wf, wf.Args()[1])
+	case "sync":
+		err = action.Sync(wf)
+	case "update":
+		err = action.Update(wf)
+	default:
+		wf.Feedback.NewItem(fmt.Sprintf("Unknown action: %s", name)).
+			Icon(aw.IconError)
+	}
+
+	if err != nil {
+		wf.Feedback.NewItem(err.Error()).Icon(aw.IconError)
+	}
+
+	wf.SendFeedback()
+}
+
+func main() {
+	if len(wf.Args()) < 1 {
+		os.Exit(1)
+	}
+
+	wf.Run(run)
+}
